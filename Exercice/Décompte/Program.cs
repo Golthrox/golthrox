4f9dca754af5d4e4ecb971f7ceead0dac9eff273
Golthrox <36459918+Golthrox@users.noreply.github.com>
2018-02-15 02:21:25 +0100

    Exercice
    Exercice réalisé en C#

---
+++ Exercice/Décompte/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programme_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int nb;
+			Console.WriteLine("Bonjour veuillez entrer un nombre entier svp");
+
+			bool validationOK = int.TryParse(Console.ReadLine(), out nb);
+			if (validationOK)
+			{
+				while (nb > 0)                              // Condition pour redescendre à 0
+				{
+
+					nb--;
+					Console.WriteLine(nb);
+				}
+			}
+			else
+			{
+				Console.WriteLine("Votre chiffre n'est pas valide");
+			}
+
+			Console.ReadKey(); // Touche de Validation pour fermer la fenetre a la fin
+		}
+	}
+}
